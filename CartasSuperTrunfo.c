071f1ae69dc60d823226625db3d149f5f8c1ff30
Matheus Pessoa <matheus41.dev@gmail.com>
2025-01-17 00:17:50 +0000

    Criação da tarefa nível Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 
+float DIVIDIR(float x, float y){ // Função de divisão para reaproveitamento de código
+	float result = x/y;
+	return result;
+	
+}
+
+
 int main() {
     printf("Desafio Cartas Super Trunfo \n"); // Cabeçalho do jogo
     
@@ -12,6 +19,8 @@ int main() {
     float area;
     float pib;
     int npontosturisticos;
+    float densidade;
+	float percapta;
     
     // Cadastro das Cartas:
     
@@ -35,15 +44,21 @@ int main() {
     printf("Digite o numero de pontos turisticos da Cidade: \n");
     scanf("%d", &npontosturisticos);
     
+    densidade = DIVIDIR(populacao, area);
+    percapta = DIVIDIR(pib, populacao);
+    
     // Exibição dos Dados das Cartas:
     
     printf("----------------- CITY CARD ----------------- \n"); // Cabeçalho para melhorar a exibição
     printf("Codigo da cidade: %d\n", codcidade );
 	printf("Nome da cidade: %s\n", nome );
 	printf("Populacao da cidade: %d\n", populacao );
-	printf("Area da cidade: %f\n", area );
-	printf("Pib da cidade: %f\n", pib );
-	printf("Numero de pontos turisticos da cidade: %d\n", npontosturisticos );
+	printf("Area da cidade: %.2f\n", area );
+	printf("Pib da cidade: %.2f\n", pib );
+	printf("Numero de pontos turisticos da cidade: %d\n\n", npontosturisticos );
+	
+	printf("Densidade: %.2f\n\n", densidade);
+	printf("PIB per Capita: %.2f\n", percapta);
 	
     return 0;
 }
